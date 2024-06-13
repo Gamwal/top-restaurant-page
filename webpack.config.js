b9b1c0173443f23b9986ffbe33092a5f259cb95d
Gamaliel Adun <adungamaliel@gmail.com>
2024-06-13 10:43:08 +0100

    update config files to allow hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { type } = require('os');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    open: true,
+  },
   plugins : [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
-        test: /\.css/i,
+        test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
