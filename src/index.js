7d46a151d3c60d9b1ef974a8ea82ef8aae1fed38
Gamaliel Adun <adungamaliel@gmail.com>
2024-06-13 10:42:26 +0100

    add components for banner-box

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('It works');
+import './style.css';
+
+const contentContainer = document.getElementById("content");
+
+const banner = document.createElement('div');
+banner.id = "banner-box"
+
+const headerLine = document.createElement('h1');
+headerLine.textContent = "Gojo's Bistroe"
+
+const paragraphOne = document.createElement('p');
+paragraphOne.textContent = "Looking for a restaurant near you?";
+
+const paragraphTwo = document.createElement('p');
+paragraphTwo.textContent = "Look no further";
+
+banner.appendChild(headerLine)
+banner.appendChild(paragraphOne);
+banner.appendChild(paragraphTwo);
+
+contentContainer.appendChild(banner);
